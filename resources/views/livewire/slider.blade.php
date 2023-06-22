c9d4983038aca1b1562048cc83bb61311512fb20
Nuno Lcm <nunolcmarques@gmail.com>
2023-06-22 14:42:20 +0200

    fix the layout frontend

--- resources/views/livewire/slider.blade.php
+++ resources/views/livewire/slider.blade.php
@@ -2,13 +2,11 @@
     <div class="gallery-wrap mt-5">
         @foreach ($products as $key => $product)
             <a href="{{ route('single.product', $product->id) }}"
+                @if ($product->photos->first()) style="background-image: url({{ asset($product->photos->first()->file) }});" @endif
                 class="item
                 @if ($key == 0) col-12
                 @elseif ($key <= 1) d-none d-sm-block col-sm-6 col-md-6
                 @else col-lg-2 d-none d-lg-block @endif">
-                @if ($product->photos->first())
-                    <div style="background-image: url({{ asset($product->photos->first()->file) }});"></div>
-                @endif
                 <p class="item__product">{{ $product->name }}</p>
             </a>
         @endforeach
